0001cfb60c77af054059d6496d65eea49a96d61f
VladimirDedov <vldedov@bk.ru>
2022-09-19 16:01:27 +0600

    translating xls to sqlite with SQL

---
+++ main.py
@@ -0,0 +1,54 @@
+import openpyxl
+import sqlite3
+
+
+def readXLS(data_name: 'name file.xlsx'):
+    """read file data.xlsx"""
+    list_id, list_request, list_go, list_person, list_email = [], [], [], [], []
+    count = 1
+    try:
+        book = openpyxl.open(data_name, read_only=True)
+        sheet = book.worksheets[0]  # first list in book
+    except:
+        print('Не удалось открыть файл data.xlsx')
+    for row in range(1, sheet.max_row + 1):
+        list_id.append(count)
+        list_request.append(sheet[row][0].value)
+        list_go.append(sheet[row][1].value)
+        list_person.append(sheet[row][2].value)
+        list_email.append(sheet[row][3].value)
+        count += 1
+    book.close()
+    return list_id, list_request, list_go, list_person, list_email
+
+
+def return_data():
+    """return one record from array list_data"""
+    list_data = readXLS('data.xlsx')
+    lst_tuple = list(zip(list_data[0], list_data[1], list_data[2], list_data[3], list_data[4]))
+
+    return lst_tuple
+
+
+def main():
+    conn = sqlite3.connect('sd.db')  # connected for sd.db or create BD
+    cur = conn.cursor()  # for sql requests in BD
+    cur.execute("""CREATE TABLE IF NOT EXISTS sd(
+        go_id INT PRIMARY KEY,
+        request TEXT,
+        go TEXT,
+        fio TEXT,
+        e_mail TEXT
+    )
+    """)
+    conn.commit()
+
+    tmp_lst = return_data()
+
+    cur.executemany("INSERT INTO sd VALUES(?,?,?,?,?)", tmp_lst)
+    conn.commit()
+
+    print("Данные успешно перенесены в БД sd.db")
+
+if __name__ == "__main__":
+    main()
